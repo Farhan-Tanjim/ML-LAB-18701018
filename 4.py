b5768274961939019f98a48f48e2ec37b799a46f
unknown <tanjim18701018@gmail.com>
2023-06-17 10:47:18 +0600

    Code Uploaded

--- 4.py
+++ 4.py
@@ -1,41 +1,29 @@
 import numpy as np
 
-def predict(X, weights, bias):
-    """
-    Make a prediction given input X and weights
-    """
-    z = np.dot(X, weights) + bias
-    return np.where(z >= 0, 1, -1)
-
-def train_weights(X, y, weights, bias, learning_rate, epochs):
-    """
-    Train the weights using the Perceptron rule
-    """
-    for _ in range(epochs):
-        for i in range(X.shape[0]):
-            prediction = predict(X[i], weights, bias)
-            if prediction != y[i]:
-                weights += learning_rate * y[i] * X[i]
-                bias += learning_rate * y[i]
-    return weights, bias
-
-# Input vectors
+# Define training data
 X = np.array([[2, 2], [1, -2], [-2, 2], [-1, 1]])
-# Labels
-y = np.array([1, 1, -1, -1])
-# Initial weights and bias
-weights = np.zeros(X.shape[1])
-bias = 0
-# Learning rate and number of epochs
-learning_rate = 1
-epochs = 100
+y = np.array([0, 1, 0, 1])
 
-weights, bias = train_weights(X, y, weights, bias, learning_rate, epochs)
-# Print the weight and bias values
-print("Weights:", weights)
-print("Bias:", bias)
+# Define initial weight and bias
+w = np.array([0, 0])
+b = 0
 
-# Make a prediction for the input vector [2 2]
-input_vector = np.array([-2, 2])
-prediction = predict(input_vector, weights, bias)
-print("Prediction for [2, 2]: ", prediction)
+# Define hardlimit function
+def hardlim(x):
+    return 1 if x >= 0 else 0
+
+# Train the perceptron
+r = np.zeros(y.shape)
+print(r)
+while not np.array_equal(r, y):
+    for i in range(len(X)):
+        a = hardlim(np.dot(w, X[i]) + b)
+        r[i] = a
+        if a != y[i]:
+            e = y[i] - a
+            w = w + e * X[i]
+            b = b + e
+
+# Print final weight and bias
+print(f'Final weight: {w}')
+print(f'Final bias: {b}')
